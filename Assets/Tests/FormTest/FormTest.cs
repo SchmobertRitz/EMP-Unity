9ec068909cdcdf8ad1692f7f98b702aa7124e20b
stoppelkopp <stoppelwoppel@gmail.com>
2017-08-03 00:08:48 +0200

    Added toggle component

--- Assets/Tests/FormTest/FormTest.cs
+++ Assets/Tests/FormTest/FormTest.cs
@@ -10,24 +10,23 @@ public class FormTest : MonoBehaviour {
 	void Start () {
         form = new Form();
         form.Spacing = 5;
-        Linear l1 = new Linear(Linear.EOrientation.Vertical);
-        l1.Spacing = 5;
-        
-        l1.Add(new Button("A1"));
-        l1.Add(new Button("A2"));
-        l1.Add(new Button("A3"));
-        l1.FixedWidth = 100;
-
-        TextField textField = new TextField("Test");
-        form.Add(l1);
-        form.Add(new Button("B"));
-        form.Add(new Button("C", b => Debug.Log(textField.Text)));
-        form.Add(textField);
+        Label label = new Label("Name of Component:");
+        label.FixedWidth = 180;
+        TextField textField = new TextField();
+        Linear componentName = Linear.Horizontal().Add(label).Add(textField);
+        componentName.FixedHeight = 25;
+        form.Add(componentName);
+        form.Add(new Label("Generate following folders in component:"));
+        form.Add(new Toggle(true, "Scripts"));
+        form.Add(new Toggle(true, "Meshes"));
+        form.Add(new Toggle(true, "Materials"));
+        form.Add(new Toggle(true, "Prefabs"));
+        form.Add(new Toggle(true, "Ressoures"));
     }
 
     void OnGUI()
     {
-        Rect rect = new Rect(0, 0, Screen.width/2, Screen.height / 2);
+        Rect rect = new Rect(0, 0, Screen.width/2, 180);
         form.OnGUI(rect);
     }
 
